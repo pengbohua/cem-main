e80d56e5982aea69491c116a9867b7aaedfe1d98
pengbohua <marvinbp1119@gmail.com>
2026-01-20 03:35:12 +0800

    configure traveling birds ready to run

--- run_exp.sh
+++ run_exp.sh
@@ -1,5 +1,6 @@
 export PYTHONUNBUFFERED=1
 export TF_ENABLE_ONEDNN_OPTS=0
-python -m experiments.run_eval -c experiments/configs/celeba_cem2.yaml --rerun
+python -m experiments.run_experiments -c experiments/configs/cub.yaml
+python -m experiments.run_experiments -c experiments/configs/travelingbirds.yaml
 #python -m experiments.run_eval -c experiments/configs/celeba_cem1.yaml --rerun
 
